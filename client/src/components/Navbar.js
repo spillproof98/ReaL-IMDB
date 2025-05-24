bda9cfb3d55ee1021aa452278ea36997cbba19f0
AdithyaR <adithya@slooh.com>
2025-05-24 13:23:35 +0530

    fix

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,22 +2,24 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import logo from '../Assets/images/homelogo.png';
 
+const base = '/ReaL-IMDB/#';
+
 export default function Navbar({ isLoggedIn, onLogout }) {
   return (
     <nav className="navbar">
-      <a href="/#/"><img src={logo} alt="IMDb Logo" height="30" /></a>
+      <a href={`${base}/`}><img src={logo} alt="IMDb Logo" height="30" /></a>
       <SearchBar />
       <div className="nav-links">
-        <a href="/#/">Home</a>
+        <a href={`${base}/`}>Home</a>
         {isLoggedIn ? (
           <>
-            <a href="/#/add">Upload Movie</a>
+            <a href={`${base}/add`}>Upload Movie</a>
             <button onClick={onLogout}>Logout</button>
           </>
         ) : (
           <>
-            <a href="/#/login">Login</a>
-            <a href="/#/signup">Sign Up</a>
+            <a href={`${base}/login`}>Login</a>
+            <a href={`${base}/signup`}>Sign Up</a>
           </>
         )}
       </div>
