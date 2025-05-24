e7f2e04e3235eb320ff089ce157bcca6abd7a40d
AdithyaR <adithya@slooh.com>
2025-05-24 12:40:41 +0530

    fix

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,16 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import App from './App';
 import './Assets/styles/imdb.css';
+import { HashRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>
 );
