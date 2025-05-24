868ab3bd7d8428eca558bc1d614c15b87ea9e55b
AdithyaR <adithya@slooh.com>
2025-05-24 12:17:26 +0530

    fix

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -7,12 +7,12 @@ import tmdbReducer from './slices/tmdbSlice';
 import searchReducer from './slices/searchSlice'
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    movie: movieReducer,
-    actors: actorReducer,
-    producers: producerReducer,
-    tmdb: tmdbReducer,
-    search: searchReducer,
-  },
+reducer: {
+  auth: authReducer,
+  movie: movieReducer,
+  actors: actorReducer,
+  producers: producerReducer,
+  tmdb: tmdbReducer,
+  search: searchReducer,
+},
 });
